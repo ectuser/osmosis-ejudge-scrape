dabaa862ec55a6a15c99ddb2aa62276831c56d9b
ectuser <m.ectuser@gmail.com>
2019-08-21 15:10:08 +0700

    All list of programms

--- main.js
+++ main.js
@@ -1,8 +1,9 @@
 const puppeteer = require('puppeteer');
+const fs = require('fs');
 (async () => {
-    var results = []
+    let results = []
     try{
-        const browser = await puppeteer.launch({headless: false});
+        const browser = await puppeteer.launch({headless: true});
         const page = await browser.newPage();
         await page.goto('https://accounts.tsu.ru/Account/Login2/?applicationId=1015');
 
@@ -11,14 +12,16 @@ const puppeteer = require('puppeteer');
         await page.$eval('.btn.btn-success', el => el.click());
 
         await page.goto('https://ejudge.kreosoft.ru/user/results');
-        var trs = await page.$$('table[border="1"] > tbody > tr');
+        let trs = await page.$$('table[border="1"] > tbody > tr');
+        console.log(trs.length);
+        // console.log(trs);
         // console.log(td)
         // try{
         //     const name = await tdName.$eval('a', e => e.innerText);
         //     console.log(name)
-        //     var link = await tdLinkToCodesPage.$eval('a', e => e.href);
+        //     let link = await tdLinkToCodesPage.$eval('a', e => e.href);
         //     console.log(link)
-        //     var code = await getCode(page, link)
+        //     let code = await getCode(page, link)
 
         //     await page.goBack();
         //     await page.goBack();
@@ -28,65 +31,128 @@ const puppeteer = require('puppeteer');
         //     console.log(e)
         // }
 
-        for (tr of trs){
-            await page.waitForNavigation();
-            var tds = await tr.$$('td')
+        // for (tr of trs){
+        for (let i = 1; i < trs.length; i++){
+            await console.log(i, "we are here")
+            let tr = await trs[i]
+            let tds = await tr.$$('td')
             await console.log(tds.length)
-            var tdName = await tds[1];
-            var tdLinkToCodesPage = await tds[2];
+            let tdName = await tds[1];
+            let tdLinkToCodesPage = await tds[2];
             // console.log(td)
             try{
                 const name = await tdName.$eval('a', e => e.innerText);
                 await console.log(name)
-                var link = await tdLinkToCodesPage.$eval('a', e => e.href);
+                let link = await tdLinkToCodesPage.$eval('a', e => e.href);
                 // console.log(link)
-                var code = await getCode(page, link)
+
+                let values = await getCode(page, link);
+
+                if (values[0] === "no data"){
+                    continue;
+                }
+
+                let code = await values[0];
                 await console.log(code)
-                
-                // results.push({
-                //     name: name,
-                // })
+                await console.log(values[1]);
+
+                let fileExtension = await whichLanguage(values[1]);
+                let fileRoute = await "tmp/" + name + fileExtension;
+                await console.log(fileRoute);
+                await fs.writeFileSync(fileRoute, code)
 
                 await page.goBack();
                 await page.goBack();
-    
+                // await page.goto('https://ejudge.kreosoft.ru/user/results');
+                trs = await page.$$('table[border="1"] > tbody > tr');
+
+                // tr = await trs[3 + 1]
+                // await console.log('here1')
+                // tds = await tr.$$('td')
+                // await console.log('here2')
+                // tdName = await tds[1];
+                // await console.log('here3', tdName.innerHTML)
+                // tdLinkToCodesPage = await tds[2];
+                // await console.log('here4')
+                // // console.log(td)
+                // name = await tdName.$eval('a', e => e.innerText);
+                // await console.log('here5')
+                // await console.log(name)
+                // link = await tdLinkToCodesPage.$eval('a', e => e.href);
+                // // console.log(link)
+                // code = await getCode(page, link)
+                // await console.log(code)
+                // trs = await page.$$('table[border="1"] > tbody > tr');
+                // // console.log(trs);
     
             }catch(e){
                 // console.log(e)
             }
         }
+        await console.log("That's all!");
 
 
 
-    } catch(err){
+    } 
+    catch(err){
         console.log(err);
     }
 })();
 
 async function getCode(page, url){
     await page.goto(url);
-    var trs = await page.$$('body > div > div > div > table > tbody > tr');
+    let trs = await page.$$('body > div > div > div > table > tbody > tr');
     for (tr of trs){
-        var tds = await tr.$$('td')
-        await console.log(tds.length)
-        var tdVerdict = await tds[3]
-        var tdLinkToCode = await tds[5]
+        let tds = await this.tr.$$('td');
+        await console.log(tds.length);
+        // let language = await tds[1];
+        // let languageText = await this.page.evaluate(el => el.innerText, language);
+        let tdVerdict = await tds[3]
+        let tdLinkToCode = await tds[5]
         try {
-            var verdict = await tdVerdict.$eval('span', e => e.innerText);
+            let verdict = await tdVerdict.$eval('span', e => e.innerText);
             if (verdict === 'Accepted'){
-                var linkToCode = await tdLinkToCode.$eval('a', e => e.href);
+                let linkToCode = await tdLinkToCode.$eval('a', e => e.href);
                 await console.log(linkToCode)
-                
+
+                let language = await tr.$eval("td:nth-child(2)", e => e.innerText);
+
                 await page.goto(linkToCode)
 
-                var code = await page.$eval('code', e => e.innerText);
-                return await code;
+                let code = await page.$eval('code', e => e.innerText);
+                return await [code, language];
             }
 
 
         } catch (error) {
             // console.log(error)
+            return ["no data", "no data"];
         }
     }
-    return 'no data'
+    return ["no data", "no data"];
+
+}
+
+async function whichLanguage(language){
+    if (language.indexOf("C++") !== -1){
+        return await ".cpp";
+    }
+    else if (language === "C#"){
+        return await ".cs";
+    }
+    else if (language.indexOf('Python') !== -1){
+        return await ".py";
+    }
+    else if (language.indexOf('Pascal') !== -1){
+        return await ".pas";
+    }
+    else if (language.indexOf('Java') !== -1){
+        return await ".java";
+    }
+    else if (language.indexOf("Ruby") !== -1){
+        return await ".rb";
+    }
+    else {
+        return await ".txt"
+    }
 }
